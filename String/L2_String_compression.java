6e1c4186621393ad2b2a279c62188606f0202657
Ritesh  Gaikwad <95370359+Riteshgaikwad@users.noreply.github.com>
2023-02-21 12:42:06 +0530

    Update L2_String_compression.java

--- String/L2_String_compression.java
+++ String/L2_String_compression.java
@@ -2,7 +2,9 @@
 Input ("aaabbcccdd" ) -->Output ("a3b2c3d2")
 */
 //Solution:-
-public class basic {
+public class basic { 
+    
+    //method-1 using String
     public static String compress(String str){
         String newStr="";
         for(int i=0;i<str.length();i++){
@@ -17,6 +19,23 @@ public class basic {
             }
         }
         return newStr;
+    }
+    
+    //method-2 using StringBuilder
+    public static StringBuilder compress_addd(String str){
+        StringBuilder sb=new StringBuilder("");
+        for(int i=0;i<str.length();i++){
+            Integer count=1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
+            }
+        }
+        return sb;
     }
     public static void main(String args[]) {
       String str="aaabbcccdd";
